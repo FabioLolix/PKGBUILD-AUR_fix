ee050f2ef889ff46080444421fba29a6e48379c6
FabioLolix <fabio.loli@disroot.org>
2023-06-13 19:28:46 +0200

    Update u.sh

--- u.sh
+++ u.sh
@@ -711,6 +711,11 @@ VAR2=s
     makepkg --printsrcinfo > .SRCINFO
     rsync {PKGBUILD,.SRCINFO} "$DEST/$VAR2/$VAR"
 
+  VAR=sol2-git
+  cd "$BASE/$VAR2/$VAR"
+    makepkg --printsrcinfo > .SRCINFO
+    rsync {PKGBUILD,.SRCINFO} "$DEST/$VAR2/$VAR"
+
   VAR=spacefm
   cd "$BASE/$VAR2/$VAR"
     makepkg --printsrcinfo > .SRCINFO
